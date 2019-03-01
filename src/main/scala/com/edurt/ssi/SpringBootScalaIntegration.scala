3a4185a29a5349b8e4b8c0fb43249ec5c4552713
shicheng <shicheng@ttxit.com>
2019-03-01 16:32:20 +0800

    add application main

---
+++ src/main/scala/com/edurt/ssi/SpringBootScalaIntegration.scala
@@ -0,0 +1,13 @@
+package com.edurt.ssi
+
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+
+@SpringBootApplication
+class SpringBootScalaIntegration
+
+object SpringBootScalaIntegration extends App{
+
+  SpringApplication.run(classOf[SpringBootScalaIntegration])
+
+}
